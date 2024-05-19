b9d017db40ab1b1f7764fd06ab5c356b6fbbe2c9
Kemvaniii <redok1706@gmail.com>
2024-05-19 14:15:13 +0700

    sua lai authentiaction

--- RoomRental/RoomRental/settings.py
+++ RoomRental/RoomRental/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.contrib.rest_framework.OAuth2Authentication'
-    )
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ),
 }
 
 MIDDLEWARE = [
