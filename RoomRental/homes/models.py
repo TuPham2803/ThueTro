15a75aa4b36ebb12f5005cdfb3b05376b684287d
TuPham2803 <tupham2832003@gmail.com>
2024-05-19 21:55:51 +0700

    email user not null

--- RoomRental/homes/models.py
+++ RoomRental/homes/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
         ('tenant', 'Người thuê trọ'),
         ('admin', 'Quản trị viên'),
     )
+    email = models.EmailField(null=False)
     user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
 
 
