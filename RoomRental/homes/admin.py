849b9fbacf1edd2f7ee34817987a5e015b4f7bd3
Kemvaniii <redok1706@gmail.com>
2024-05-20 10:44:16 +0700

    thêm cloudinary, thêm trường avatar user

--- RoomRental/homes/admin.py
+++ RoomRental/homes/admin.py
@@ -1,16 +1,41 @@
 from django.contrib import admin
-from .models import User, PostAccommodation, AccommodationImage, InteractionAccommodation, Follow, PostRequest
+from .models import User, PostAccommodation, AccommodationImage, InteractionAccommodation, Follow, PostRequest, \
+    UserAvatarImage
+import cloudinary
+from django.utils.html import mark_safe
 
 
 class AccommodationImageInline(admin.TabularInline):
     model = AccommodationImage
 
 
+class UserAvatarImageInline(admin.TabularInline):
+    model = UserAvatarImage
+
+
+class MyUserAdmin(admin.ModelAdmin):
+    inlines = [UserAvatarImageInline]
+
+    def my_image(self, instance):
+        if instance:
+            if instance.image is cloudinary.CloudinaryResource:
+                return mark_safe(f"<img width='120' src='{instance.image.url}' />")
+
+            return mark_safe(f"<img width='120' src='/static/{instance.image.name}' />")
+
+
 class AccommodationAdmin(admin.ModelAdmin):
     inlines = [AccommodationImageInline]
 
+    def my_image(self, instance):
+        if instance:
+            if instance.image is cloudinary.CloudinaryResource:
+                return mark_safe(f"<img width='120' src='{instance.image.url}' />")
 
-admin.site.register(User)
+            return mark_safe(f"<img width='120' src='/static/{instance.image.name}' />")
+
+
+admin.site.register(User, MyUserAdmin)
 admin.site.register(PostAccommodation, AccommodationAdmin)
 admin.site.register(PostRequest)
 # admin.site.register(Comment)
--- RoomRental/homes/models.py
+++ RoomRental/homes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from ckeditor.fields import RichTextField
+from cloudinary.models import CloudinaryField
 
 
 class User(AbstractUser):
@@ -11,6 +12,11 @@ class User(AbstractUser):
     )
     email = models.EmailField(null=False)
     user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+
+
+class UserAvatarImage(models.Model):
+    image = CloudinaryField(null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class BaseModel(models.Model):
@@ -50,7 +56,8 @@ class PostAccommodation(BaseModel):
 
 
 class AccommodationImage(models.Model):
-    image = models.ImageField(upload_to='homes/%Y/%m')
+    # image = models.ImageField(upload_to='homes/%Y/%m')
+    image = CloudinaryField(null=True)
     caption = models.CharField(max_length=100, blank=True)
     post_accommodation = models.ForeignKey(PostAccommodation, on_delete=models.CASCADE)
 
