83e0a25253c18bdbf70b2a2007938fbb5b7e7c42
TuPham2803 <tupham2832003@gmail.com>
2024-06-01 07:59:40 +0700

    feat: view review post

--- RoomRental/homes/views.py
+++ RoomRental/homes/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import PostAccommodation, User, PostRequest, CommentAccommodation, CommentRequest, LikeAccommodation, \
-    LikeRequest, AccommodationImage
+    LikeRequest, AccommodationImage, PendingStatus
 from . import serializers, perms
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -54,7 +54,18 @@ class PostAccommodationViewSet(viewsets.ViewSet, generics.ListCreateAPIView, gen
 
         return Response(serializers.LikeAccommodationSerializer(li).data,
                         status=status.HTTP_201_CREATED)
-
+        
+    @action(methods=['POST'], url_path='review', detail=True)
+    def review(self, request, pk):
+        post = self.get_object()
+        data = request.data
+        review = data.get('review')
+        if review not in ['Approved', 'Failed']:
+            return Response({'error': 'Reivew must is Approved or Failed'}, status=status.HTTP_400_BAD_REQUEST)
+        post.pending_status = review=='Approved' and PendingStatus.APPROVED or PendingStatus.FAILED
+        post.save()
+        
+        return Response(serializers.PostAccommodationSerializer(post).data, status=status.HTTP_200_OK)
     def get_queryset(self):
         queryset = self.queryset
 
