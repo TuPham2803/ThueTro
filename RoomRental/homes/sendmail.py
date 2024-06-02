a49faed3334c8abd8b2f80b93d752511d7138b1f
TuPham2803 <tupham2832003@gmail.com>
2024-06-02 21:54:06 +0700

    refactor: implement .env to backend

--- RoomRental/homes/sendmail.py
+++ RoomRental/homes/sendmail.py
@@ -4,23 +4,26 @@ from email.mime.text import MIMEText
 from django.template.loader import render_to_string
 from .models import Follow
 from .serializers import PostAccommodationSerializer
-mailtrap_host = "sandbox.smtp.mailtrap.io"
-mailtrap_port = 2525
-mailtrap_username = "ddfe90a83625e9"
-mailtrap_password = "9a7e1248dd5c50"
-mailtrap_sender = "roomrental@gmail.com"
+from decouple import config
+mailtrap_host = config('MAILTRAP_HOST')
+mailtrap_port = config('MAILTRAP_PORT')
+mailtrap_username = config('MAILTRAP_USERNAME')
+mailtrap_password = config('MAILTRAP_PASSWORD')
+mailtrap_sender = config('MAILTRAP_SENDER')
 
 
 def send_mail(post_accommodation):
     # Retrieve the followers queryset
-    followers = Follow.objects.filter(owner=post_accommodation.user_post).select_related('follower')
+    followers = Follow.objects.filter(
+        owner=post_accommodation.user_post).select_related('follower')
 
     for follow in followers:
         user = follow.follower
         post_data = PostAccommodationSerializer(post_accommodation).data
 
         # Render email content from template
-        email_content = render_to_string('mail/new_post.html', {"post_data": post_data, "user": user})
+        email_content = render_to_string(
+            'mail/new_post.html', {"post_data": post_data, "user": user})
 
         # Send email
         with smtplib.SMTP(mailtrap_host, mailtrap_port) as server:
